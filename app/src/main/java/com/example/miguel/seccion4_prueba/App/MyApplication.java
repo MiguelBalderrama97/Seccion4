b34716e47d4b204493af30693a9f304e3f1280bb
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-01-05 00:02:40 -0700

    Configuración por defecto
    Implementación del metodo setUpRealmConfig en la clase MyApplication para establecer la configuración por defecto.

--- app/src/main/java/com/example/miguel/seccion4_prueba/App/MyApplication.java
+++ app/src/main/java/com/example/miguel/seccion4_prueba/App/MyApplication.java
@@ -8,6 +8,7 @@ import com.example.miguel.seccion4_prueba.Models.Note;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmObject;
 import io.realm.RealmResults;
 
@@ -19,10 +20,16 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        setUpRealmConfig();
         Realm realm = Realm.getDefaultInstance();
         BoardID = getIdByTable(realm, Board.class);
         NoteID = getIdByTable(realm, Note.class);
         realm.close();
+    }
+
+    private void setUpRealmConfig(){
+        RealmConfiguration config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
+        Realm.setDefaultConfiguration(config);
     }
 
 //    METODO PARA AUTOINCREMENT
