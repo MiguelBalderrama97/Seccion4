29139b9bceb0060375c17ea40e6ee64a06e8f965
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-01-06 11:25:14 -0700

    AlertDialog
    Agregar el FAB en el BoardActivity  y que salga un alert al principio, creación de un layout para inflar el alert.

--- app/src/main/java/com/example/miguel/seccion4_prueba/Activities/BoardActivity.java
+++ app/src/main/java/com/example/miguel/seccion4_prueba/Activities/BoardActivity.java
@@ -1,15 +1,63 @@
 package com.example.miguel.seccion4_prueba.Activities;
 
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.miguel.seccion4_prueba.R;
 
 public class BoardActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        fab = findViewById(R.id.fabAddBoard);
+
+        showAlertForCreatingBoard("Title", "Message");
+    }
+
+    private void showAlertForCreatingBoard(String title, String msg){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        if(title != null){
+            builder.setTitle(title);
+        }
+        if(msg != null){
+            builder.setMessage(msg);
+        }
+
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_create_board, null);
+        builder.setView(view);
+
+        final EditText input = view.findViewById(R.id.editTextNewBoard);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String boardName = input.getText().toString().trim();
+                if(boardName.length() > 0){
+                    createNewBoard(boardName);
+                }else{
+                    Toast.makeText(BoardActivity.this, "The name is required to create a new Board", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void createNewBoard(String boardName){
+
     }
 }
